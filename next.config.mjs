28fcc89487de66c03974871c9be0003ff890d3ba
Thomas Guillet <thomas@palya.fr>
2025-01-23 09:19:31 +0100

    Ajuste basePath au déploiement GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = "production" === process.env.NODE_ENV;
+
 const nextConfig = {
+  basePath: isProduction ? "/grist-previsionnel" : "",
   output: "export",
   reactStrictMode: true,
 };
