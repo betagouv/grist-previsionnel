5fe7fcf89d00ff5b8e8c631f1a35c1f418d9aee9
Thomas Guillet <thomas@palya.fr>
2025-05-05 17:55:30 +0200

    Ajoute un widget demo du bug #1594

---
+++ pages/inspect-on-records.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import jsonwebtoken from "jsonwebtoken";
+
+export default function InspectOnRecords() {
+  const [dump, setDump] = useState({})
+
+  useEffect(() => {
+    window.grist.ready({
+      requiredAccess: "full"
+    });
+    window.grist.onRecords(async (records) => {
+      const tokenInfo = await grist.docApi.getAccessToken({readOnly: true});
+      const tableId = await window.grist.getTable().getTableId();
+
+      const url = `${tokenInfo.baseUrl}/tables/${tableId}/records?auth=${tokenInfo.token}`;
+      const response = await fetch(url)
+      setDump({
+        response: await response.json(),
+        token: jsonwebtoken.decode(tokenInfo.token)
+      });
+    });
+  }, []);
+
+  return <div>
+  <h1>TEST</h1>
+  <pre>{JSON.stringify(dump, null, 2)}</pre>
+  </div>;
+}
