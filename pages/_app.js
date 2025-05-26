55612b34e394e78c0eec99e4251b70067c844de8
Thomas Guillet <thomas@palya.fr>
2025-05-26 18:49:46 +0200

    Déplace le contenu

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "@/styles/globals.css";
-import "handsontable/styles/handsontable.min.css";
-import "handsontable/styles/ht-theme-main.min.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
